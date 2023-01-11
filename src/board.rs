25999ed246c00c4ddf869ffed55879faf1081d6f
MrDumling <doubleefoster@gmail.com>
2023-01-11 17:26:39 -0500

    finished up FEN strings, needs testing

--- src/board.rs
+++ src/board.rs
@@ -1,12 +1,18 @@
-use crate::{piece::Piece, Side};
+use crate::{piece::{Piece, PieceType}, Side};
 
 pub struct Board {
-    pub held_pieces: [Option<Piece>; 64],
-    pub active_side: Side,
-    pub caslting_capabilities: CastlingCapablities,
+    held_pieces: [Option<Piece>; 64],
+    active_side: Side,
+    caslting_capabilities: CastlingCapablities,
+    en_passent_target: Option<usize>
 }
 
-pub struct CastlingCapablities {
+struct BoardPosition {
+    rank: u8,
+    file: u8,
+}
+
+struct CastlingCapablities {
     white_caslting_ability: CasltingAbilities,
     black_caslting_ability: CasltingAbilities,
 }
@@ -27,6 +33,107 @@ impl From<&str> for CastlingCapablities {
                 king_side: x.contains('K'),
                 queen_side: x.contains('Q'),
             },
+        }
+    }
+}
+
+impl Board {
+    pub fn default_position() -> Self {
+        Board::position_from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    }
+
+    pub fn position_from_fen(input_fen: &str) -> Self {
+        fn get_piece_from_char(c: char) -> Piece {
+            Piece {
+                side: if c.is_uppercase() {
+                    Side::WHITE
+                } else {
+                    Side::BLACK
+                },
+                piece_type: match c.to_ascii_lowercase() {
+                    'r' => PieceType::Rook,
+                    'n' => PieceType::Knight,
+                    'b' => PieceType::Bishop,
+                    'k' => PieceType::King,
+                    'q' => PieceType::Queen,
+                    'p' => PieceType::Pawn,
+                    x => {
+                        panic!("Unknown letter '{x}'")
+                    }
+                },
+            }
+        }
+
+        let [piece_positions, active_side, castling_capabilities, en_passant_target_pos, _, _]: [&str; 6] = input_fen
+            .split_whitespace()
+            .collect::<Vec<&str>>()
+            .try_into()
+            .expect("Invalid FEN string {input_position}");
+
+        let active_side = match active_side {
+            "w" => Side::WHITE,
+            "b" => Side::BLACK,
+            _ => {
+                panic!("Invalid side in FEN string {active_side}")
+            }
+        };
+
+        const EMPTY: Option<Piece> = None;
+        let mut held_pieces: [Option<Piece>; 64] = [EMPTY; 64];
+
+        let mut rank = 7;
+        let mut file = 0;
+        for current_letter in piece_positions.chars() {
+            if current_letter == '/' {
+                rank -= 1;
+                file = 0;
+            } else if current_letter.is_numeric() {
+                file += current_letter as usize - '0' as usize;
+            } else {
+                held_pieces[(rank) * 8 + file] = Some(get_piece_from_char(current_letter));
+                file += 1;
+            }
+        }
+
+        Board {
+            held_pieces,
+            active_side,
+            caslting_capabilities: castling_capabilities.into(),
+            en_passent_target: if en_passant_target_pos == "-" {None} else {Some(Self::position_to_index(en_passant_target_pos))}
+        }
+    }
+
+    fn position_to_index(pos: &str) -> usize {
+        if pos.len() != 2 {
+            panic!("Expected position of length 2, instead got {pos}");
+        }
+
+        let mut chars = pos.chars();
+
+        let rank = chars.next().unwrap() as usize - 'a' as usize;
+        let file = chars.next().unwrap().to_digit(8).unwrap() as usize;
+
+        rank * 8 + file
+    }
+
+    pub fn display_board(&self) {
+        for (index, piece) in self.held_pieces.iter().enumerate() {
+            if index % 8 == 0 {
+                println!();
+            }
+
+            print!("{}", match piece {
+                Some(x) => {
+                    x.get_display()
+                },
+                None => {
+                    if ((index / 8) + (index % 8)) % 2 == 0 {
+                        ' '
+                    } else {
+                        '█'
+                    }
+                }
+            });
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::io;
-mod piece;
+use std::io::{self};
 mod board;
+mod piece;
 
-use crate::{piece::*, board::*};
+use crate::board::*;
 
 fn main() {
     let board = select_mode();
@@ -21,103 +21,19 @@ fn select_mode() -> Board {
             .read_line(&mut provided_request)
             .expect("Failed to read request");
 
-        if provided_request.starts_with("1")
+        if provided_request.starts_with('1')
             || provided_request.to_lowercase().starts_with("normal")
         {
             return Board::default_position();
-        } else if provided_request.starts_with("2")
+        } else if provided_request.starts_with('2')
             || provided_request.to_lowercase().starts_with("position")
         {
-            return Board::position_from_fen(todo!());
+            println!("Please provide a FEN string");
+            let mut provided_request = String::new();
+            io::stdin().read_line(&mut provided_request).expect("Failed to read FEN string");
+            return Board::position_from_fen(&provided_request[..]);
         } else {
             println!("Invalid request, please choose from valid requests:");
-        }
-    }
-}
-
-impl Board {
-    fn default_position() -> Self {
-        Board::position_from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-    }
-
-    fn position_from_fen(input_position: &str) -> Self {
-        fn get_piece_from_char(c: char) -> Piece {
-            Piece {
-                side: if c.is_uppercase() {
-                    Side::WHITE
-                } else {
-                    Side::BLACK
-                },
-                piece_type: match c.to_ascii_lowercase() {
-                    'r' => PieceType::Rook,
-                    'n' => PieceType::Knight,
-                    'b' => PieceType::Bishop,
-                    'k' => PieceType::King,
-                    'q' => PieceType::Queen,
-                    'p' => PieceType::Pawn,
-                    x => {
-                        panic!("Unknown letter '{x}'")
-                    }
-                },
-            }
-        }
-
-        let [piece_positions, active_side, castling_capabilities, en_passant_target, _, _]: [&str; 6] = input_position
-            .split_whitespace()
-            .collect::<Vec<&str>>()
-            .try_into()
-            .expect("Invalid FEN string {input_position}");
-
-        let active_side = match active_side {
-            "w" => Side::WHITE,
-            "b" => Side::BLACK,
-            _ => {
-                panic!("Invalid side in FEN string {active_side}")
-            }
-        };
-
-        const EMPTY: Option<Piece> = None;
-        let mut held_pieces: [Option<Piece>; 64] = [EMPTY; 64];
-
-        let mut rank = 7;
-        let mut file = 0;
-        for current_letter in piece_positions.chars() {
-            if current_letter == '/' {
-                rank -= 1;
-                file = 0;
-            } else if current_letter.is_numeric() {
-                file += current_letter as usize - '0' as usize;
-            } else {
-                held_pieces[(rank) * 8 + file] = Some(get_piece_from_char(current_letter));
-                file += 1;
-            }
-        }
-
-        Board {
-            held_pieces,
-            active_side,
-            caslting_capabilities: castling_capabilities.into(),
-        }
-    }
-
-    fn display_board(&self) {
-        for (index, piece) in self.held_pieces.iter().enumerate() {
-            if index % 8 == 0 {
-                println!();
-            }
-
-            print!("{}", match piece {
-                Some(x) => {
-                    x.get_display()
-                },
-                None => {
-                    if ((index / 8) + (index % 8)) % 2 == 0 {
-                        ' '
-                    } else {
-                        '█'
-                    }
-                }
-            });
         }
     }
 }
